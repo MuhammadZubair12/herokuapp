72b95e417806fc466945de6e394c3c3a43da2862
Muhammad Zubair <zubairmazhar23@gmail.com>
2020-12-28 09:00:29 +0500

    add Dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
-  {path:"signup", component:SignupComponent}
+  {path:"signup", component:SignupComponent},
+  {path:"", component:DashboardComponent}
 ];
 
 @NgModule({
